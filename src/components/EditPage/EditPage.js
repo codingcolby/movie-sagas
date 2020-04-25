5affde182bf305a2a6448627945ee286d8caea9c
allthethings <learn2code@respectdesfonds.com>
2020-04-24 20:03:08 -0500

    added shell of edit page

---
+++ src/components/EditPage/EditPage.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import axios from "axios";
+import "./EditPage.css";
+
+class EditPage extends Component {
+	render() {
+		return (
+			<div className="EditDiv">
+				<p className="Announce">Coming Soon!</p>
+				<p> Edit Page</p>
+			</div>
+		);
+	}
+}
+
+export default EditPage;
