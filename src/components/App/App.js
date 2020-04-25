ef5ec00f2e1edf1282fb90f3f125e73678be8d26
allthethings <learn2code@respectdesfonds.com>
2020-04-24 19:53:55 -0500

    parked placeholders for movie inventory and detail; added simply styling while in dev

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import MovieInventory from "../MovieInventory/MovieInventory";
+import MovieDetail from "../MovieDetail/MovieDetail";
 
 class App extends Component {
-  // Renders the entire app on the DOM
-  render() {
-    return (
-      <div className="App">
-        <p>Empty Page</p>
-      </div>
-    );
-  }
+	// Renders the entire app on the DOM
+	render() {
+		return (
+			<div className="App">
+				<h1>Movie Night Options!</h1>
+				<div className="Temp_align">
+					<MovieInventory />
+					<MovieDetail />
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
