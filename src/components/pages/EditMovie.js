761d90cdcf67cfb4c05c10d8d6171b4e70374514
allthethings <learn2code@respectdesfonds.com>
2020-04-25 22:18:12 -0500

    added state, change and click handlers; incorporated some groundwork for stretch populating of fields

--- src/components/pages/EditMovie.js
+++ src/components/pages/EditMovie.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import axios from "axios";
-import "./sytles/EditMovie.css";
+import "../styles/EditMovie.css";
 
 // !!! PRACTICE BRANCHING
 
@@ -11,31 +10,77 @@ import "./sytles/EditMovie.css";
 // STRETCH - add genre(s?) to movie
 // STRETCH - remove genre(s?) from movie
 
-function* cancel_detailReturn() {
-	// no changes, nav to MovieDetail.js
-}
+class EditMovie extends Component {
+	state = {
+		title: "",
+		description: "",
+	};
 
-function* saveMovieEditToDB_detailReturn() {
-	// save title and description input to DB, nav to MovieDetail.js
-}
+	// modify and use later if input and textarea will autopopulate based on movie_id when page loads for title only, or title and description
+	// componentDidMount() {
+	// 	this.props.dispatch({ type: "READ" });
+	// }
 
-// Refer to the user's movie selection to establish visual continuity when page loads - see line 21
+	handleChangeTitle = (event) => {
+		this.setState({
+			title: event.target.value,
+		});
+	};
 
-class EditMoviePage extends Component {
+	handleChangeDescription = (event) => {
+		this.setState({
+			description: event.target.value,
+		});
+	};
+
+	// no changes: clears input, navs to MovieDetail.js
+	cancel_detailReturnClick = (event) => {
+		this.setState({
+			title: "",
+			description: "",
+		});
+		// nav to MovieDetail.js
+		this.props.history.push("/MovieDetail");
+	};
+
+	// save title and description input to DB, clears input and textarea, navs to MovieDetail.js
+	saveMovieEditToDB_detailReturnClick = (event) => {
+		this.props.dispatch({
+			type: "UPDATE",
+			payload: this.state,
+		});
+		this.setState({
+			title: "",
+			description: "",
+		});
+		this.props.history.push("/MovieDetail");
+	};
+
+	// Show user's movie selected title to establish visual continuity when page loads - see line 67
+
 	render() {
 		return (
 			<div className="EditDiv">
-				<p className="Announce">Coming Soon!</p>
-				<p> Edit Page for [selected movie title]</p>
-				<input>Change Title - Stretch pre-populate with current</input>
-				<textarea>
-					Change Description - Stretch pre-populate with current
+				<p className="Announce">
+					Edit Page for {this.redux.store.moviesReducer.title}
+				</p>
+				<input
+					onChange="this.handleChangeTitle"
+					type="text"
+					default={this.redux.store.moviesReducer.title}
+				/>
+				<textarea onChange="this.handleChangeDescription" type="text">
+					{this.redux.store.moviesReducer.description}
 				</textarea>
-				<button onClick="cancel_detailReturn">Cancel</button>
-				<button onClick="saveMovieEditToDB_detailReturn">Save</button>
+				<button onClick={this.cancel_detailReturnClick}>Cancel</button>
+				<button onClick={this.saveMovieEditToDB_detailReturnClick}>Save</button>
 			</div>
 		);
 	}
 }
 
-export default EditMoviePage;
+const mapStoreToRedux = (store) => ({
+	store,
+});
+
+export default connect(mapStoreToRedux)(EditMovie);
