467113f75d8815719edeaea58aa31b714453359c
allthethings <learn2code@respectdesfonds.com>
2020-04-26 20:56:13 -0500

    added as landing page

---
+++ src/components/pages/Vault.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import MovieList from "./MovieList";
+import Vault from "../pages/Vault";
+
+const vault = (props) => (
+	<div>
+		<h2>Movie Night Potential Picks</h2>
+		<Vault />
+	</div>
+);
+
+export default vault;
